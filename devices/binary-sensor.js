58ec615c92eca9d7f48ce267880b47b3d0b0a9e1
Tom Sightler <tom.sightler@veeam.com>
2024-06-21 16:46:43 -0400

    Revert to upstream ring-client-api

--- devices/binary-sensor.js
+++ devices/binary-sensor.js
@@ -1,5 +1,5 @@
 import RingSocketDevice from './base-socket-device.js'
-import { RingDeviceType } from '@tsightler/ring-client-api'
+import { RingDeviceType } from 'ring-client-api'
 
 // Helper functions
 function chirpToMqttState(chirp) {
